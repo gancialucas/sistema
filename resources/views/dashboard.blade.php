35544e5c19bcab030b3856bed1ca70a477c062a0
gancialucas <lucasgancia@gmail.com>
2023-10-23 19:08:01 -0300

    CHANGED: section position for a dinamic profile view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.plantilla')
     @section('contenido')
+                <li class="profile">
+                    <div class="profile-details">
+                        <img src="https://drive.google.com/uc?export=view&id=1ETZYgPpWbbBtpJnhi42_IR3vOwSOpR4z" alt="profileImg">
+                        <div class="name_job">
+                            <div class="name">Stella Army</div>
+                            <div class="job">Web designer</div>
+                        </div>
+                    </div>
 
-        <div class="text">Bienvenido al Dashboard</div>
+                    <i class='bx bx-log-out' id="log_out"></i>
+                </li>
+            </ul>
+        </div>
 
+        <section class="home-section">
+            <div class="text">Bienvenido al Dashboard</div>
     @endsection()
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -76,19 +76,3 @@
                 <span class="tooltip">Setting</span>
             </li>
 
-            <li class="profile">
-                <div class="profile-details">
-                    <img src="https://drive.google.com/uc?export=view&id=1ETZYgPpWbbBtpJnhi42_IR3vOwSOpR4z"
-                        alt="profileImg">
-                    <div class="name_job">
-                        <div class="name">Stella Army</div>
-                        <div class="job">Web designer</div>
-                    </div>
-                </div>
-
-                <i class='bx bx-log-out' id="log_out"></i>
-            </li>
-        </ul>
-    </div>
-
-    <section class="home-section">
