89a9bf6f45bbf7bbd1f84f2823b1c4649dc16162
gancialucas <lucasgancia@gmail.com>
2023-10-29 18:37:42 -0300

    MODIFIED: web uri for a correct use

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,7 +10,17 @@ Route::get('/', function () {
 
 /*
 |--------------------------------------------------------------------------
-| CRUD de Usuarios
+| CONFIG de 'LOGIN'
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/login', [UserController::class, 'index']);
+Route::get('/user/validation', [UserController::class, 'validation']);
+Route::get('/user/store', [UserController::class, 'store']);
+
+/*
+|--------------------------------------------------------------------------
+| CONFIG de 'DASHBOARD'
 |--------------------------------------------------------------------------
 */
 
