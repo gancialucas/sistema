bf88d81a79d48aae9c026cb0856d6ae9c615df65
gancialucas <lucasgancia@gmail.com>
2024-01-25 18:32:04 -0300

    RECHANGED: to tailwinds styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,9 +11,8 @@
     {{-- Favicon --}}
     <link rel="shortcut icon" href="public/imgs/utilities/logo.png" type="image/x-icon">
 
-    {{-- Scripts - Bootstrap & SweetAlert --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    {{-- Scripts - Tailwind & SweetAlert --}}
+    <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     {{-- Link - CSS --}}
@@ -21,7 +20,7 @@
 </head>
 
 <body>
-    <div class="bg-withe overflow-hidden">
+    <div class="overflow-hidden">
         <div class="relative isolate px-6 pt-14 lg:px-8">
             <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
                 aria-hidden="true">
@@ -31,40 +30,29 @@
             </div>
 
             <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-                <div class="hidden sm:mb-8 sm:flex sm:justify-center">
+                <div class="hidden sm:mb-4 sm:flex sm:justify-center">
                     <div class="flex items-center text-sm underline underline-offset-2 leading-6 text-gray-600">
-                        <span class="mr-1 font-semibold">¡Hola!</span><img class="w-auto h-4"
-                            src="/imgs/utilities/hand_shake.png" alt="hand_shake">
+                        <span class="mr-1 font-semibold">¡Hola!</span><img class="w-auto h-4" src="/imgs/utilities/hand_shake.png" alt="hand_shake">
                     </div>
                 </div>
 
                 <div class="text-center">
-                    <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Ven y conoce</h1>
-
-                    <div class="grid place-items-center pt-5">
-                        <img src="/imgs/utilities/logo.png" alt="logo">
-                    </div>
+                    <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">¡Bienvenido!</h1>
 
                     <p class="mt-6 text-lg leading-8 text-gray-600">
-                        ¡Bienvenido! Soy Lucas, un <span class="font-bold text-blue-400">desarrollador web</span>
+                        Soy Lucas, un <span class="font-bold text-orange-400">desarrollador web</span>
                         apasionado por aprender y expandir mis conocimientos. Este proyecto tiene como propósito la
-                        <span class="font-bold text-blue-400">creación de un CRUD y un dashboard completamente
+                        <span class="font-bold text-blue-400">creación de un sistema de gestión completo con un dashboard completamente
                             funcional.</span>
                     </p>
 
                     <div class="mt-10 flex items-center justify-center gap-x-6">
-                        <a href="/login"
-                            class="text-sm font-semibold leading-6 text-gray-900 rounded-full px-5 py-1 text-sm leading-6 text-gray-600 ring-2 ring-gray-900/10 hover:ring-gray-900/20">Conocer
-                            más <span aria-hidden="true">→</span></a>
+                        <a href="/login" class="text-sm font-semibold leading-6 text-gray-900 rounded-full px-5 py-1 text-sm leading-6 text-gray-600 ring-2 ring-gray-900/10 hover:ring-gray-900/20">Conocer más <span aria-hidden="true">→</span></a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
 </body>
 
 </html>
